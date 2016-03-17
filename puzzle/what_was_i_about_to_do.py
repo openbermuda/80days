fba08edbe2e4991ea171743c347a687993d4c6d9
John Gill <swfiua@gmail.com>
2016-03-16 23:25:24 -0300

    first puzzle

---
+++ puzzle/what_was_i_about_to_do.py
@@ -0,0 +1,21 @@
+
+class Z:
+
+    def dtest(infile):
+
+        for x in self.generate(infile):
+            for a in x:
+                yield a
+
+
+    def btest(infile):
+
+        for x in infile:
+
+            if a == '#':
+                yield a
+            else:
+
+            
+
+    
